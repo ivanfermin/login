1015bb33af84983fa5786eb5ee8c0b13d5e1d073
ivanfermin <106822032+ivanfermin@users.noreply.github.com>
2022-10-21 09:55:26 -0400

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
             "arom","202108942","comtraceña");
     Docente javier= new Docente("ivaan","fersmin","encsinas","714790879","johadn goete",
             "araom","2021089942","comtracleña");
-
+    Persona[]personas={javier};
+    Usuario[]usuarios={javier};
+    Persona persona=javier;
 }
